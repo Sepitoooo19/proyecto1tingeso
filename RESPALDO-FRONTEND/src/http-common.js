4d32222c7b86e33c58ed3d5be8efea7bcec42f63
Benjamin Sepulveda Flores <benjamin.sepulveda@usach.cl>
2024-11-03 23:15:33 -0300

    front modified

--- RESPALDO-FRONTEND/src/http-common.js
+++ RESPALDO-FRONTEND/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const prestaBancoServer = `prestabanco-app.westus2.cloudapp.azure.com`;
-const prestaBancoPort = `80`;
+const prestaBancoPort = 80;
 
 console.log(prestaBancoServer);
 console.log(prestaBancoPort);
--- RESPALDO-FRONTEND/src/services/creditapplication.service.js
+++ RESPALDO-FRONTEND/src/services/creditapplication.service.js
@@ -2,7 +2,7 @@ import httpClient from '../http-common';
 
 const createCreditApplicationByRut = (rut, loanType) => {
     console.log("Rut:", rut, "Loan Type:", loanType);
-    return httpClient.post(`/api/v1/credit_application/create`, { 
+    return httpClient.post(`http://prestabanco-app.westus2.cloudapp.azure.com/api/v1/credit_application/create`, { 
         rut: rut, 
         loan_type: loanType 
     });
